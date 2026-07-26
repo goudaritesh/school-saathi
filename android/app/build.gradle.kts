88418ee984ae71e82db940d0828467c39b771927
goudaritesh <goudariteshkumar1@gmail.com>
2026-07-26 19:02:28 +0530

    chore: remove secrets from repository

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,15 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+import java.util.Properties
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { localProperties.load(it) }
+}
+val mapsApiKey = localProperties.getProperty("MAPS_API_KEY", "")
+
 android {
     namespace = "com.example.school_sathi"
     compileSdk = 36
@@ -30,6 +39,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
     }
 
     buildTypes {
